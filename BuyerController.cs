3ecc641079a3df038cbf10443041797db8196e74
Anjalimishraspn <anjali.mishra@compunnel.com>
2017-04-20 09:48:06 +0530

    Add files via upload

---
+++ BuyerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyProject123.Models;
+using MyProject123.ViewModels;
+
+using System.Data;
+using System.Data.Entity;
+
+using System.Net;
+
+
+using MyProject123.DAL;
+using log4net;
+
+namespace MyProject123.Controllers
+{
+    public class BuyerController : Controller
+    {
+        //
+        // GET: /Buyer/
+        MyProject123.DAL.MyContextClass db = new MyProject123.DAL.MyContextClass();
+        public ActionResult BIndex()
+        {
+            BuyerAndBookAndSellerViewModel bb = new BuyerAndBookAndSellerViewModel();
+            bb.BookAndSellerMapDisplays = getBookAndSellers();
+            bb.SliderContents = getSlider();
+
+            return View(bb);
+        }
+
+        public List<SliderContent> getSlider()
+        {                 
+            var list = db.SliderContents.OrderBy(x => x.SliderContentId).ToList();
+            return list;
+        }
+
+
+        public List<BookAndSellerMapDisplay> getBookAndSellers()
+        {
+
+         
+            var bookandsellermapsprop = db.BookAndSellerMapDisplays.Include(b => b.BookAndSellerMap.Book).Include(b => b.BookAndSellerMap.BookFormat).Include(b => b.BookAndSellerMap.Language).Include(b => b.BookAndSellerMap.Seller).OrderByDescending(n => n.BookAndSellerMap.CreatedOn);
+            return bookandsellermapsprop.ToList();
+        }
+
+
+
+    }
+}
+
+       
